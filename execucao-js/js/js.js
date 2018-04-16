da77ab8461bb4ef98276499f203576d77d8582ba
odaircori <odaircori@gmail.com>
2018-04-16 15:38:02 -0300

    apagar pasta

--- execucao-js/js/js.js
+++
@@ -1,8 +0,0 @@
-console.log("Início programação síncrona");
-
-//função time assíncrona
-setTimeout(
-    console.log("Executando programação assíncrona"), 2000)
-
-console.log("Término programação síncrona");
-
